f6b5a119258418d0b2951b80eae6c84b65b6e7a8
Maya Bowman <mayashady1@gmail.com>
2019-11-13 11:32:45 -0500

    configurations

--- src/app.js
+++ src/app.js
@@ -14,10 +14,19 @@ const morganOption = (NODE_ENV === 'production'
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors({
-  origin: CLIENT_ORIGIN 
-  })
-)
+
+const whitelist = [CLIENT_ORIGIN, 'http://localhost:3000']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions))
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
-const express = require('express')
-const app = express()
+const knex = require('knex')
+// const express = require('express')
+const app = require('./app')
 
-const { PORT } = require('./config')
+const { PORT, DATABASE_URL } = require('./config')
+
+const db = knex({
+  client: 'pg',
+  connection: DATABASE_URL
+})
+
+app.set('db', db)
 
 app.get('/api/*', (req, res) => {
   res.json({ok: true})
