6ecdf58d393515d28a9a0dd00bc73009a464e292
Maya Bowman <mayashady1@gmail.com>
2019-11-13 10:17:02 -0500

    set up temporary catch-all endpoint

--- src/server.js
+++ src/server.js
@@ -1,6 +1,22 @@
-const app = require('./app')
+const express = require('express')
+const app = express()
+
 const { PORT } = require('./config')
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+app.get('/api/*', (req, res) => {
+  res.json({ok: true})
 })
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+
+module.exports = {app}
+
+
+
+// original boilerplate code
+// const app = require('./app')
+// const { PORT } = require('./config')
+
+// app.listen(PORT, () => {
+//   console.log(`Server listening at http://localhost:${PORT}`)
+// })
