0d6a3ddda4187966479f27bc4bfed10efe1e6c7e
Maya Bowman <mayashady1@gmail.com>
2019-11-24 10:13:32 -0500

    implemented postgrator and refactored config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
-module.exports = { 
+module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL,
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN
 }
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,6 +1,16 @@
 const app = require('../src/app')
+const knex = require('knex')
 
 describe('App', () => {
+  let db
+
+  before(() => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DATABASE_URL,
+    })
+  })
+
   it('GET / responds with 200 containing "Hello, world!"', () => {
     return supertest(app)
       .get('/')
