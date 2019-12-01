ab5be756beae931b46f2404164d944575534628a
Maya Bowman <mayashady1@gmail.com>
2019-12-01 13:50:48 -0500

    worked on testing

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -6,13 +6,13 @@ const helpers = require('./test-helpers')
 describe('Auth Endpoints', function() {
   let db
 
-  const { testUsers } = helpers.makeArticlesFixtures()
+  const { testUsers } = helpers.makeFixtures()
   const testUser = testUsers[0]
 
   before('make knex instance', () => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL,
+      connection: process.env.TEST_DATABASE_URL,
     })
     app.set('db', db)
   })
--- test/test-helpers.js
+++ test/test-helpers.js
@@ -23,7 +23,7 @@ function makeUsersArray() {
       id: 3,
       user_email: 'testuser3@gmail.com',
       first_name: 'Test3',
-      last_name: 'Usere',
+      last_name: 'User3',
       password: 'password',
       date_created: '2029-01-22T16:28:32.615Z'
     },
@@ -36,4 +36,57 @@ function makeUsersArray() {
       date_created: '2029-01-22T16:28:32.615Z'
     },
   ]
+}
+
+function makeFixtures() {
+  const testUsers = makeUsersArray()
+
+  return { testUsers }
+}
+
+function cleanTables(db) {
+  return db.transaction(trx =>
+    trx.raw(
+      `TRUNCATE
+        bookery_users,
+      `
+    )
+    .then(() =>
+      Promise.all([
+        trx.raw(`ALTER SEQUENCE bookery_users_id_seq minvalue 0 START WITH 1`),
+        trx.raw(`SELECT setval('bookery_users_id_seq', 0)`)
+      ])
+    )
+  )
+}
+
+function seedUsers(db, users) {
+  const preppedUsers = users.map(user => ({
+    ...user,
+    password: bcrypt.hashSync(user.password, 1)
+  }))
+  return db.into('bookery_users').insert(preppedUsers)
+    .then(() =>
+      // update the auto sequence to stay in sync
+      db.raw(
+        `SELECT setval('bookery_users_id_seq', ?)`,
+        [users[users.length - 1].id],
+      )
+    )
+}
+
+function makeAuthHeader(user, secret = process.env.JWT_SECRET) {
+  const token = jwt.sign({ user_id: user.id }, secret, {
+    subject: user.user_email,
+    algorithm: 'HS256',
+  })
+  return `Bearer ${token}`
+}
+
+module.exports = {
+  makeUsersArray,
+  makeFixtures,
+  cleanTables,
+  seedUsers,
+  makeAuthHeader
 }
