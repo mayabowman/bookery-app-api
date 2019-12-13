46087e66ed8cb60610166a8b6bee0fe9ab1cac9a
Maya Bowman <mayashady1@gmail.com>
2019-12-13 12:50:07 -0500

    working on cors issue

--- src/app.js
+++ src/app.js
@@ -31,6 +31,11 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // app.use(cors(
 //   { origin: CLIENT_ORIGIN }
