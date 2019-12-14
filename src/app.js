a1fe0c5d8365f6881d06ba34ba1796eb79e92b71
Maya Bowman <mayashady1@gmail.com>
2019-12-13 20:44:10 -0500

    worked on cors issue

--- src/app.js
+++ src/app.js
@@ -18,24 +18,25 @@ const morganOption = (NODE_ENV === 'production'
 
 app.use(morgan(morganOption))
 app.use(helmet())
+app.use(cors())
 
-const whitelist = [CLIENT_ORIGIN, 'http://localhost:3000']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// const whitelist = [CLIENT_ORIGIN, 'http://localhost:3000']
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
-app.use(cors(corsOptions))
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(cors(corsOptions))
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 // app.use(cors(
 //   { origin: CLIENT_ORIGIN }
