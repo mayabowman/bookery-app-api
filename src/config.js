5019344f1d0a7cd2cab50549dbf0b684ee5cc3cc
Maya Bowman <mayashady1@gmail.com>
2019-12-18 12:37:25 -0500

    working on deploy

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/bookery',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000",
+  // API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000",
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
 }
