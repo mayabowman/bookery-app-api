2b0b1424d747ba01a212eec6131fb81438735c0d
Maya Bowman <mayashady1@gmail.com>
2020-01-13 12:02:26 -0500

    cleaned up code and switched config for deploy

--- src/app.js
+++ src/app.js
@@ -1,46 +1,37 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV, CLIENT_ORIGIN } = require('./config')
-const usersRouter = require('./users/users-router')
-const booksRouter = require('./books/books-router')
-const authRouter = require('./auth/auth-router')
-const bookshelfRouter = require('./bookshelf/bookshelf-router')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config');
+const usersRouter = require('./users/users-router');
+const booksRouter = require('./books/books-router');
+const authRouter = require('./auth/auth-router');
+const bookshelfRouter = require('./bookshelf/bookshelf-router');
 
 
-const app = express()
+const app = express();
 
 const morganOption = (NODE_ENV === 'production'
   ? 'tiny'
-  : 'common')
+  : 'common');
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
-// app.use(cors(
-//   {
-//     "origin": CLIENT_ORIGIN,
-//     "methods": "GET,POST,PATCH,DELETE"
-//   }
-// ))
-
-// app.options('*', cors())
-
-app.use('/api/users', usersRouter)
-app.use('/api/books', booksRouter)
-app.use('/api/auth', authRouter)
-app.use('/api/bookshelf', bookshelfRouter)
+app.use('/api/users', usersRouter);
+app.use('/api/books', booksRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/bookshelf', bookshelfRouter);
 
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
-})
+});
 
 app.use(function errorHandler(error, req, res, next) {
-  let response
+  let response;
   if (NODE_ENV === 'production') {
     response = { error: { message: 'server error' } }
   } else {
@@ -48,6 +39,6 @@ app.use(function errorHandler(error, req, res, next) {
     response = { message: error.message, error }
   }
   res.status(500).json(response)
-})
+});
 
-module.exports = app
+module.exports = app;
